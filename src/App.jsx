9cb1d23701556e48218cf64fd5596cf7f0c2e399
Mario Gutiérrez <mariogutierrezcto@gmail.com>
2026-01-07 13:43:37 +0100

    Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,17 @@ import GetInTouch from './components/GetInTouch/GetInTouch'
 import Header from './components/Header/Header'
 import Carousel from './components/Carousel/Carousel'
 import { Toaster } from 'sonner'
+import { useState, useEffect } from 'react'
+import { BeatLoader } from "react-spinners";
+
+const override = {
+  display: "block",
+  margin: "auto",
+  borderColor: "white",
+};
 
 function App() {
+  const [spinner, setSpinner] = useState(true)
   const OPTIONS = { loop: true }
   const IMAGES = [
     '/media/gallery/first_year_award.jpg',
@@ -18,19 +27,39 @@ function App() {
     '/media/gallery/indesiahack_group_winners.jpg'
   ]
 
+  useEffect(() =>
+    setTimeout(() => {
+      setSpinner(false)
+    }, 1000)
+  , [])
   return (
     <>
-      <Toaster position="top-center" />
-      <Header></Header>
-      <div id='content'>
-        <Experience></Experience>
-        <Carousel images={IMAGES} options={OPTIONS}></Carousel>
-        <Portfolio></Portfolio>
-        <Technologies></Technologies>
-        <AboutMe></AboutMe>
-        <GetInTouch></GetInTouch>
-      </div>
-      <Footer></Footer>
+      { spinner ? 
+        <div className='spinner-center'>
+          <BeatLoader
+            color={"#ffffffff"}
+            loading={spinner}
+            cssOverride={override}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      :
+      <>
+        <Toaster position="top-center" />
+        <Header></Header>
+        <div id='content'>
+          <Experience></Experience>
+          <Carousel images={IMAGES} options={OPTIONS}></Carousel>
+          <Portfolio></Portfolio>
+          <Technologies></Technologies>
+          <AboutMe></AboutMe>
+          <GetInTouch></GetInTouch>
+        </div>
+        <Footer></Footer>
+      </>
+      }
     </>
   )
 }
